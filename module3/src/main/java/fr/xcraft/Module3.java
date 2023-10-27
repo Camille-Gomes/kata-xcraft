931414d469f602223c22da515172f02bba53aaea
Danot Yann <yann.danot@shodo.io>
2023-10-27 15:37:10 +0200

    start implementation module 3

--- module3/src/main/java/fr/xcraft/Module3.java
+++ module3/src/main/java/fr/xcraft/Module3.java
@@ -1,8 +1,28 @@
 package fr.xcraft;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Module3 {
 
     public static String process(String input) {
+        String chiffrement =
+                "key1 [x2]\n" +
+                "key2\n" +
+                "key1 [x1]"; // TODO PARSE JSON
+        String[] lines = chiffrement.split("\n");
+
+//        Arrays.stream(lines).flatMap((line) -> {
+//            if(!line.contains("[")) {
+//                return List.of(line);
+//            }
+//
+//            List<String> strings = Arrays.stream(line.split(" \\[")).toList();
+//            return strings;
+//
+//        } );
+
+
         return "";
     }
 
